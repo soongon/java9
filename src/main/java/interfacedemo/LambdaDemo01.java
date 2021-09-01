f27aa9791981ab50c116081bbeab7007da780b39
soongon <soongon@hucloud.co.kr>
2021-09-01 14:08:58 +0900

    initial commit

--- src/main/java/interfacedemo/LambdaDemo01.java
+++ src/main/java/interfacedemo/LambdaDemo01.java
@@ -3,17 +3,15 @@ package interfacedemo;
 public class LambdaDemo01 {
     public static void main(String[] args) {
         LambdaDemo01 demo = new LambdaDemo01();
-        System.out.println(demo.calcNumber(new FFIImpl(), 2, 3));
+        System.out.println(demo.calcNumber(new FirstFunctionalInterface() {
+            @Override
+            public int calc(int a, int b) {
+                return a * a - 3 + b;
+            }
+        }, 2, 3));
     }
     public int calcNumber(FirstFunctionalInterface ffi, int a, int b) {
         return ffi.calc(a, b);
-    }
-
-    static class FFIImpl implements FirstFunctionalInterface {
-        @Override
-        public int calc(int a, int b) {
-            return a * 2 + b;
-        }
     }
 }
 
