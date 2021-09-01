ba0f12b97744cd53169ab684cd885efc6135fdd7
soongon <soongon@hucloud.co.kr>
2021-09-01 17:09:51 +0900

    initial commit

---
+++ src/main/java/apple/Apple.java
@@ -0,0 +1,25 @@
+package apple;
+
+public class Apple {
+    private int weight;
+    private String color;
+
+    public Apple() {}
+    public Apple(int weight, String color) {
+        this.weight = weight;
+        this.color = color;
+    }
+    public int getWeight() {
+        return weight;
+    }
+    public String getColor() {
+        return color;
+    }
+    @Override
+    public String toString() {
+        return "Apple{" +
+                "weight=" + weight +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/apple/AppleClassifier.java
@@ -0,0 +1,45 @@
+package apple;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class AppleClassifier {
+
+    public static void main(String[] args) {
+        List<Apple> apples = Arrays.asList(
+                new Apple(250, "green"),
+                new Apple(350, "red"),
+                new Apple(550, "green"),
+                new Apple(150, "red"),
+                new Apple(450, "green"),
+                new Apple(250, "green"),
+                new Apple(550, "red")
+        );
+
+        // 1. 300 그램 이상인 사과를 필터링
+//        List<Apple> appleOver300 = new ArrayList<>();
+//        for (Apple apple : apples) {
+//            if (apple.getWeight() > 300) {
+//                appleOver300.add(apple);
+//            }
+//        }
+
+        Predicate<Apple> condition = a -> a.getColor().equals("red") && a.getWeight() > 300;
+
+        List<Apple> result = filterApples(apples, condition);
+
+        System.out.println(result);
+    }
+
+    public static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> predicate) {
+        List<Apple> newList = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (predicate.test(apple)) {
+                newList.add(apple);
+            }
+        }
+        return newList;
+    }
+}
