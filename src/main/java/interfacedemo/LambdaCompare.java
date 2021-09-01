08f4e9cf3d64bf57745c571e9870d6de1ea0913a
soongon <soongon@hucloud.co.kr>
2021-09-01 15:04:32 +0900

    initial commit

---
+++ src/main/java/interfacedemo/LambdaCompare.java
@@ -0,0 +1,24 @@
+package interfacedemo;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaCompare {
+    public static void main(String[] args) {
+        List<String> favoriteColors =
+                Arrays.asList("red", "pink", "blue", "brown", "purple");
+
+        favoriteColors.sort(new MyComparator());
+        System.out.println(favoriteColors);
+    }
+}
+
+// 1. anonymous inner class version
+class MyComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        return o2.length() - o1.length();
+    }
+}
