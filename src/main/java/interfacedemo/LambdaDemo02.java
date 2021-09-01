e3e54bdb60bbee718f2272be484ee9edabb03a95
soongon <soongon@hucloud.co.kr>
2021-09-01 14:16:29 +0900

    initial commit

---
+++ src/main/java/interfacedemo/LambdaDemo02.java
@@ -0,0 +1,23 @@
+package interfacedemo;
+
+public class LambdaDemo02 {
+    public static void main(String[] args) {
+        LambdaDemo02 demo = new LambdaDemo02();
+        demo.calcNumber((int a, int b) -> a * a + b, 5, 7);
+
+    }
+    public int calcNumber(FirstFunctionalInterface02 ffi, int a, int b) {
+        return ffi.calc(a, b);
+    }
+}
+
+@FunctionalInterface
+interface FirstFunctionalInterface02 {
+    int calc(int a, int b);
+    default int add(int a, int b) {
+        return a + b;
+    }
+    static int sub(int a, int b) {
+        return a -b;
+    }
+}
